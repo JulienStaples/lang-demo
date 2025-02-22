8f3b55c478f685839981c0f9000ae73bb7f632c3
JulienStaples <julienstaples.web@gmail.com>
2025-02-21 20:51:49 -0700

    sml: add VirtPage values to page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
 "use server"
 
+import VirtPage from "./components/VirtPage"
+import { dummyText } from "./lib/constants/constants"
+
 export default async function Home() {
-  return <main className=" bg-gray-950 h-[667px] w-[375px]"></main>
+  return (
+    <main className=" bg-gray-950 h-[667px] w-[375px] text-[1.2rem]">
+      <VirtPage startingText={dummyText[2]}></VirtPage>
+    </main>
+  )
 }
