223a29eb4edd580d228c74a08eb73e63e8fd8172
JulienStaples <julienstaples.web@gmail.com>
2025-02-21 21:11:55 -0700

    feat: add page functionality

--- app/components/VirtPage.js
+++ app/components/VirtPage.js
@@ -1,10 +1,15 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useMemo } from "react"
+import nlpObj from "compromise"
+import fnlpObj from "fr-compromise"
 
 export default function VirtPage(startingText) {
   const [text, setText] = useState(startingText.startingText)
-
+  const [page, setPage] = useState(0)
+  const pages = useMemo(() => {
+    return textToPages(text)
+  }, [text])
   const [view, setView] = useState(readingView)
 
   function toggleView() {
@@ -12,7 +17,29 @@ export default function VirtPage(startingText) {
   }
 
   function pageNext() {}
+
   function pagePrev() {}
+
+  function textToPages(text) {
+    let nlp = nlpObj(text)
+    let pages = []
+    let page = []
+    let wordIndex = 0
+
+    nlp.termList().forEach((word, i) => {
+      if (wordIndex < 79) {
+        page.push(word)
+        wordIndex++
+      } else {
+        page.push(word)
+        pages.push(page)
+        page = []
+        wordIndex = 0
+      }
+    })
+    pages.push(page)
+    return pages
+  }
 
   return (
     <div className="w-full h-full flex justify-center items-end overflow-visible">
