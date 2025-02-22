93ef529fd2c451fe9a93a69071262619952dc904
JulienStaples <julienstaples.web@gmail.com>
2025-02-21 20:58:12 -0700

    feat: add skeleton for VirtPage

--- app/components/VirtPage.js
+++ app/components/VirtPage.js
@@ -1,5 +1,27 @@
-import React from "react"
+"use client"
 
-export default function VirtPage() {
-  return <div>VirtPage</div>
+import { useState } from "react"
+
+export default function VirtPage(startingText) {
+  const [view, setView] = useState(readingView)
+
+  function toggleView() {}
+  function pageNext() {}
+  function pagePrev() {}
+
+  return (
+    <div className="w-full h-full flex justify-center items-end overflow-visible">
+      <div className="flex flex-col items-end w-[80%] h-[97%] gap-3 overflow-visible">
+        <button onClick={toggleView}>=</button>
+        <div className="w-full h-full">{view}</div>
+        <div className=" flex gap-7">
+          <button onClick={pagePrev}>{`<`}</button>
+          <button onClick={pageNext}>{`>`}</button>
+        </div>
+      </div>
+    </div>
+  )
+
+  function editView() {}
+  function readingView() {}
 }
