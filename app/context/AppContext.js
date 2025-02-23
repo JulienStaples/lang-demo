7b016e3618adb5a70601bc803aeb799b0c699a52
JulienStaples <julienstaples.web@gmail.com>
2025-02-23 15:19:29 -0700

    Feat: add functionality to AppContext.js
    this allows both VirtPage.js and translateTab.js to communicate changes
    to display the flyout menu

--- app/context/AppContext.js
+++ app/context/AppContext.js
@@ -1,13 +1,18 @@
 "use client"
+
 import { createContext, useState } from "react"
 
 export const AppContext = createContext()
 
 export default function AppProvider({ children }) {
-  const [showFlyout, setShowFlyout] = useState("HELLO")
+  const [showFlyout, setShowFlyout] = useState(false)
+
+  function handleClick(e) {
+    setShowFlyout((prev) => !prev)
+  }
 
   return (
-    <AppContext.Provider value={{ showFlyout, setShowFlyout }}>
+    <AppContext.Provider value={{ showFlyout, handleClick }}>
       {children}
     </AppContext.Provider>
   )
