f9dc456b3e3e6d02396c04f6c7b6d534bfb2a405
JulienStaples <julienstaples.web@gmail.com>
2025-03-02 06:10:27 -0700

    Sml: Clean Word.js

--- app/components/Word.js
+++ app/components/Word.js
@@ -7,14 +7,10 @@ export default function Word(props) {
   let wordObj = props.wordObj
 
   function handleEnter(e) {
-    e.currentTarget.firstChild.dataset.hovered = "true"
-    e.currentTarget.firstChild.children[0].dataset.hovered = "true"
     e.currentTarget.firstChild.children[1].dataset.hovered = "true"
   }
 
   function handleExit(e) {
-    e.currentTarget.firstChild.dataset.hovered = "false"
-    e.currentTarget.firstChild.children[0].dataset.hovered = "false"
     e.currentTarget.firstChild.children[1].dataset.hovered = "false"
   }
 
@@ -26,17 +22,12 @@ export default function Word(props) {
         onMouseLeave={(e) => handleExit(e)}
         onClick={() => handleClick(wordObj)}
       >
-        <span
-          data-hovered="false"
-          className="cursor-pointer relative inline-block data-[hovered=true]:-translate-y-[3px] hover:invert duration-100 ease-in-out"
-        >
-          <span data-hovered="false" className="relative z-10">
-            {wordObj.text}
-          </span>
+        <span className="cursor-pointer relative inline-block hover:invert duration-100 ease-in-out">
+          <span className="relative z-10">{wordObj.text}</span>
           <span
             data-hovered="false"
             className="absolute inset-x-[-2px] inset-y-[3px] rounded-md bg-gradient-to-r from-sky-700 bg via-sky-700 to-sky-700 z-0 
-             data-[hovered=true]:from-sky-700 data-[hovered=true]:to-sky-700 data-[hovered=true]:-inset-x-1 duration-100 ease-in-out"
+             data-[hovered=true]:from-sky-700 data-[hovered=true]:to-sky-700 data-[hovered=true]:-inset-x-1 duration-100 ease-in-out data-[hovered=true]:inset-y-0"
           ></span>
         </span>
       </span>
