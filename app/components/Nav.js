c603ebee06c64d8799c92651740dda18884d5e0b
JulienStaples <julienstaples.web@gmail.com>
2025-03-04 08:39:49 -0700

    Sml: Add nav styles

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -4,7 +4,7 @@ import LangSelect from "./LangSelect"
 
 export default function Nav() {
   return (
-    <nav className="flex gap-3 justify-center">
+    <nav className=" absolute inset-5 mx-auto w-[80%] h-fit flex flex-nowrap gap-3 justify-center">
       <SampleTexts></SampleTexts>
       <LangSelect></LangSelect>
     </nav>
--- app/components/dropdown/Dropdown.js
+++ app/components/dropdown/Dropdown.js
@@ -5,7 +5,7 @@ export default function Dropdown(props) {
   const items = props.items
 
   return (
-    <div className=" group relative">
+    <div className=" group relative bg-teal-900 rounded-md px-1">
       <span>{title}</span>
       <div className=" invisible opacity-50 group-hover:opacity-100 absolute group-hover:visible bg-slate-700 w-fit z-40 duration-100 ease-in-out">
         {<DropdownItems items={items} />}
