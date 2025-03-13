4ad8dd4788746cbd11587a6609e7df8201ca14a5
JulienStaples <julienstaples.web@gmail.com>
2025-03-13 14:52:18 -0700

    Feat: Add title bar

---
+++ app/components/TitleBar.js
@@ -0,0 +1,17 @@
+"use client"
+
+import { useContext } from "react"
+import { AppContext } from "../context/AppContext"
+
+export default function TitleBar() {
+  const { presetText } = useContext(AppContext)
+
+  return (
+    <div className="flex justify-between pr-5">
+      <div className="flex gap-3">
+        <span>0</span>
+        <span>{presetText.title}</span>
+      </div>
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -3,15 +3,13 @@ import NavProvider from "./context/NavContext"
 import VirtPage from "./components/VirtPage"
 import Nav from "./components/Nav"
 import FlyoutMenu from "./components/FlyoutMenu"
+import TitleBar from "./components/TitleBar"
 
 export default async function Home() {
   return (
     <main className="relative flex h-[667px] w-[375px] bg-[#171717]">
       <div className="relative flex grow flex-col gap-4 p-4 pr-0">
-        <div className="flex gap-3">
-          <span>0</span>
-          <span>Title</span>
-        </div>
+        <TitleBar></TitleBar>
         <VirtPage></VirtPage>
       </div>
       <NavProvider>
