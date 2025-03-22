c21e845412c13b2a0fef064f55b6d028f59b607c
JulienStaples <julienstaples.web@gmail.com>
2025-03-22 02:16:36 -0700

    Sml: Add dark mode wrapper

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css"
 import AppProvider from "./context/AppContext"
 import NavProvider from "./context/NavContext"
+import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata = {
   title: "lang-demo",
@@ -9,12 +10,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`flex h-[100vh] items-center justify-center bg-black text-white antialiased`}
       >
         <AppProvider>
-          <NavProvider>{children}</NavProvider>
+          <NavProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </NavProvider>
         </AppProvider>
       </body>
     </html>
