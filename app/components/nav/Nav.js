ac396e5541f325e0cd2a0ea5445b57a3fe4230a2
JulienStaples <julienstaples.web@gmail.com>
2025-03-22 18:21:00 -0700

    Feat: Add shadcn sidebar

--- app/components/nav/Nav.js
+++ app/components/nav/Nav.js
@@ -1,155 +1,240 @@
 "use client"
 
-import { useContext, useState } from "react"
+import { useContext } from "react"
 import { NavContext } from "../../context/NavContext"
-import { ModeToggle } from "@/components/mode-toggle"
+import { AppSidebar } from "@/components/app-sidebar"
+
+import {
+  BookOpen,
+  Database,
+  Home,
+  Languages,
+  PenLine,
+  SearchIcon,
+  Settings,
+} from "lucide-react"
 
 export default function Nav() {
   const { tab, selectTab, exitAnim, setTabsPane, tabsPane } =
     useContext(NavContext)
-  const [toggleNav, setToggleNav] = useState(false)
+  // const [toggleNav, setToggleNav] = useState(false)
 
-  const navItems = genNavTabs()
+  // const navItems = genNavTabs()
+
+  const navTabObjs = [
+    {
+      icon: Home,
+      title: "Home",
+      id: "home-tab",
+      action: () => {
+        tab.key == "home-tab" && tabsPane
+          ? exitAnim()
+          : (selectTab("home"), setTabsPane(true))
+      },
+    },
+
+    {
+      icon: Settings,
+      title: "Options",
+      id: "options-tab",
+      action: () => {
+        tab.key == "options-tab" && tabsPane
+          ? exitAnim()
+          : (selectTab("options"), setTabsPane(true))
+      },
+    },
+    {
+      icon: Languages,
+      title: "Translate",
+      id: "translate-tab",
+      action: () => {
+        tab.key == "translate-tab" && tabsPane
+          ? exitAnim()
+          : (selectTab("translate"), setTabsPane(true))
+      },
+    },
+    {
+      icon: SearchIcon,
+      title: "Details",
+      id: "details-tab",
+      action: () => {
+        tab.key == "details-tab" && tabsPane
+          ? exitAnim()
+          : (selectTab("details"), setTabsPane(true))
+      },
+    },
+    {
+      icon: BookOpen,
+      title: "Texts",
+      id: "texts-tab",
+      action: () => {
+        tab.key == "texts-tab" && tabsPane
+          ? exitAnim()
+          : (selectTab("texts"), setTabsPane(true))
+      },
+    },
+    {
+      icon: PenLine,
+      title: "Langs",
+      id: "langs-tab",
+      action: () => {
+        tab.key == "langs-tab" && tabsPane
+          ? exitAnim()
+          : (selectTab("langs"), setTabsPane(true))
+      },
+    },
+    {
+      icon: Database,
+      title: "Database",
+      id: "db-tab",
+      action: () => {
+        tab.key == "db-tab" && tabsPane
+          ? exitAnim()
+          : (selectTab("db"), setTabsPane(true))
+      },
+    },
+  ]
 
   return (
-    <nav
-      className="z-10 flex select-none flex-col items-center justify-start bg-rose-900 shadow-[0px_0px_10px] shadow-black hover:z-50"
-      id="tabs-bar"
-      data-nav-active={toggleNav}
-      onClick={(e) => {
-        e.target.id == "tabs-bar" ? setToggleNav((prev) => !prev) : ""
-      }}
-    >
-      {navItems}
-      {toggleNav && <ModeToggle />}
-    </nav>
+    // <nav
+    //   className="z-10 flex select-none flex-col items-center justify-start bg-rose-900 shadow-[0px_0px_10px] shadow-black hover:z-50"
+    //   id="tabs-bar"
+    //   data-nav-active={toggleNav}
+    //   onClick={(e) => {
+    //     e.target.id == "tabs-bar" ? setToggleNav((prev) => !prev) : ""
+    //   }}
+    // >
+    //   {navItems}
+    //   {toggleNav && <ModeToggle />}
+    // </nav>
+    <AppSidebar items={navTabObjs} />
   )
 
-  function genNavTabs() {
-    const navTabObjs = [
-      {
-        icon: "^",
-        title: "Home",
-        id: "home-tab",
-        action: () => {
-          tab.key == "home-tab" && tabsPane
-            ? exitAnim()
-            : (selectTab("home"), setTabsPane(true))
-        },
-      },
+  // function genNavTabs() {
+  //   const navTabObjs = [
+  //     {
+  //       icon: "^",
+  //       title: "Home",
+  //       id: "home-tab",
+  //       action: () => {
+  //         tab.key == "home-tab" && tabsPane
+  //           ? exitAnim()
+  //           : (selectTab("home"), setTabsPane(true))
+  //       },
+  //     },
 
-      {
-        icon: "=",
-        title: "Options",
-        id: "options-tab",
-        action: () => {
-          tab.key == "options-tab" && tabsPane
-            ? exitAnim()
-            : (selectTab("options"), setTabsPane(true))
-        },
-      },
-      {
-        icon: "0",
-        title: "Translate",
-        id: "translate-tab",
-        action: () => {
-          tab.key == "translate-tab" && tabsPane
-            ? exitAnim()
-            : (selectTab("translate"), setTabsPane(true))
-        },
-      },
-      {
-        icon: "1",
-        title: "Details",
-        id: "details-tab",
-        action: () => {
-          tab.key == "details-tab" && tabsPane
-            ? exitAnim()
-            : (selectTab("details"), setTabsPane(true))
-        },
-      },
-      {
-        icon: "2",
-        title: "Texts",
-        id: "texts-tab",
-        action: () => {
-          tab.key == "texts-tab" && tabsPane
-            ? exitAnim()
-            : (selectTab("texts"), setTabsPane(true))
-        },
-      },
-      {
-        icon: "3",
-        title: "Langs",
-        id: "langs-tab",
-        action: () => {
-          tab.key == "langs-tab" && tabsPane
-            ? exitAnim()
-            : (selectTab("langs"), setTabsPane(true))
-        },
-      },
-      {
-        icon: "4",
-        title: "Database",
-        id: "db-tab",
-        action: () => {
-          tab.key == "db-tab" && tabsPane
-            ? exitAnim()
-            : (selectTab("db"), setTabsPane(true))
-        },
-      },
-    ]
+  //     {
+  //       icon: "=",
+  //       title: "Options",
+  //       id: "options-tab",
+  //       action: () => {
+  //         tab.key == "options-tab" && tabsPane
+  //           ? exitAnim()
+  //           : (selectTab("options"), setTabsPane(true))
+  //       },
+  //     },
+  //     {
+  //       icon: "0",
+  //       title: "Translate",
+  //       id: "translate-tab",
+  //       action: () => {
+  //         tab.key == "translate-tab" && tabsPane
+  //           ? exitAnim()
+  //           : (selectTab("translate"), setTabsPane(true))
+  //       },
+  //     },
+  //     {
+  //       icon: "1",
+  //       title: "Details",
+  //       id: "details-tab",
+  //       action: () => {
+  //         tab.key == "details-tab" && tabsPane
+  //           ? exitAnim()
+  //           : (selectTab("details"), setTabsPane(true))
+  //       },
+  //     },
+  //     {
+  //       icon: "2",
+  //       title: "Texts",
+  //       id: "texts-tab",
+  //       action: () => {
+  //         tab.key == "texts-tab" && tabsPane
+  //           ? exitAnim()
+  //           : (selectTab("texts"), setTabsPane(true))
+  //       },
+  //     },
+  //     {
+  //       icon: "3",
+  //       title: "Langs",
+  //       id: "langs-tab",
+  //       action: () => {
+  //         tab.key == "langs-tab" && tabsPane
+  //           ? exitAnim()
+  //           : (selectTab("langs"), setTabsPane(true))
+  //       },
+  //     },
+  //     {
+  //       icon: "4",
+  //       title: "Database",
+  //       id: "db-tab",
+  //       action: () => {
+  //         tab.key == "db-tab" && tabsPane
+  //           ? exitAnim()
+  //           : (selectTab("db"), setTabsPane(true))
+  //       },
+  //     },
+  //   ]
 
-    let navTabs = []
+  //   let navTabs = []
 
-    navTabObjs.forEach((item) => {
-      navTabs.push(
-        <div
-          key={`nav-${item.title}`}
-          onClick={item.action}
-          className="tab group relative w-full cursor-pointer p-2 text-center after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:bg-rose-950 hover:bg-rose-950 hover:shadow-[inset_0_-1px_2px] hover:shadow-black"
-        >
-          <span
-            data-nav-active={toggleNav}
-            data-tab-active={tab.key == item.id}
-            className={`tab-icon 2 relative group-hover:invisible data-[nav-active=true]:hidden data-[tab-active='true']:before:absolute data-[tab-active='true']:before:-inset-1 data-[tab-active='true']:before:-z-10 data-[tab-active='true']:before:rounded-md data-[tab-active='true']:before:bg-rose-950 data-[tab-active='true']:before:shadow-[inset_0_0_1px] data-[tab-active='true']:before:shadow-black`}
-          >
-            {item.icon}
-          </span>
+  //   navTabObjs.forEach((item) => {
+  //     navTabs.push(
+  //       <div
+  //         key={`nav-${item.title}`}
+  //         onClick={item.action}
+  //         className="tab group relative w-full cursor-pointer p-2 text-center after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:bg-rose-950 hover:bg-rose-950 hover:shadow-[inset_0_-1px_2px] hover:shadow-black"
+  //       >
+  //         <span
+  //           data-nav-active={toggleNav}
+  //           data-tab-active={tab.key == item.id}
+  //           className={`tab-icon 2 relative group-hover:invisible data-[nav-active=true]:hidden data-[tab-active='true']:before:absolute data-[tab-active='true']:before:-inset-1 data-[tab-active='true']:before:-z-10 data-[tab-active='true']:before:rounded-md data-[tab-active='true']:before:bg-rose-950 data-[tab-active='true']:before:shadow-[inset_0_0_1px] data-[tab-active='true']:before:shadow-black`}
+  //         >
+  //           {item.icon}
+  //         </span>
 
-          <div
-            data-nav-active={toggleNav}
-            data-tab-active={tab.key == item.id}
-            className={`relative hidden rounded-r-md bg-rose-900 p-2 shadow-[-1px_1px_2px] shadow-black after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:bg-gradient-to-l after:from-transparent after:to-rose-950 after:to-40% group-hover:bottom-0 group-hover:left-0 group-hover:top-0 data-[tab-active='true']:before:absolute data-[tab-active='true']:before:inset-1 data-[tab-active='true']:before:z-10 data-[tab-active='true']:before:rounded-md data-[tab-active='true']:before:bg-rose-950 data-[tab-active='true']:before:shadow-[inset_0_0_2px] data-[tab-active='true']:before:shadow-black data-[nav-active=false]:group-hover:absolute data-[nav-active=false]:group-hover:inline-block`}
-          >
-            <div className="flex gap-3">
-              <span
-                data-tab-active={tab.key == item.id}
-                className="relative z-20 w-3 after:absolute after:inset-y-0 after:-right-2 after:w-[2px] after:rounded-full after:bg-rose-950 data-[tab-active='true']:after:bg-rose-900"
-              >
-                {item.icon}
-              </span>
-              <span className="relative z-20">{item.title}</span>
-            </div>
-          </div>
+  //         <div
+  //           data-nav-active={toggleNav}
+  //           data-tab-active={tab.key == item.id}
+  //           className={`relative hidden rounded-r-md bg-rose-900 p-2 shadow-[-1px_1px_2px] shadow-black after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:bg-gradient-to-l after:from-transparent after:to-rose-950 after:to-40% group-hover:bottom-0 group-hover:left-0 group-hover:top-0 data-[tab-active='true']:before:absolute data-[tab-active='true']:before:inset-1 data-[tab-active='true']:before:z-10 data-[tab-active='true']:before:rounded-md data-[tab-active='true']:before:bg-rose-950 data-[tab-active='true']:before:shadow-[inset_0_0_2px] data-[tab-active='true']:before:shadow-black data-[nav-active=false]:group-hover:absolute data-[nav-active=false]:group-hover:inline-block`}
+  //         >
+  //           <div className="flex gap-3">
+  //             <span
+  //               data-tab-active={tab.key == item.id}
+  //               className="relative z-20 w-3 after:absolute after:inset-y-0 after:-right-2 after:w-[2px] after:rounded-full after:bg-rose-950 data-[tab-active='true']:after:bg-rose-900"
+  //             >
+  //               {item.icon}
+  //             </span>
+  //             <span className="relative z-20">{item.title}</span>
+  //           </div>
+  //         </div>
 
-          <div
-            data-nav-active={toggleNav}
-            data-tab-active={tab.key == item.id}
-            className={`relative flex gap-4 py-1 pl-1 pr-2 hover:bg-rose-950 data-[nav-active=false]:hidden data-[tab-active='true']:before:absolute data-[tab-active='true']:before:-left-[.3em] data-[tab-active='true']:before:-right-[.3em] data-[tab-active='true']:before:bottom-0 data-[tab-active='true']:before:top-0 data-[tab-active='true']:before:-z-10 data-[tab-active='true']:before:rounded-md data-[tab-active='true']:before:bg-rose-950 data-[tab-active='true']:before:shadow-[inset_0_0_2px] data-[tab-active='true']:before:shadow-black`}
-          >
-            <span
-              data-tab-active={tab.key == item.id}
-              className="relative w-3 after:absolute after:inset-y-0 after:-right-2 after:w-[2px] after:rounded-full after:bg-rose-950 after:group-hover:bg-rose-900 data-[tab-active='true']:after:bg-rose-900"
-            >
-              {item.icon}
-            </span>
-            <span>{item.title}</span>
-          </div>
-        </div>,
-      )
-    })
+  //         <div
+  //           data-nav-active={toggleNav}
+  //           data-tab-active={tab.key == item.id}
+  //           className={`relative flex gap-4 py-1 pl-1 pr-2 hover:bg-rose-950 data-[nav-active=false]:hidden data-[tab-active='true']:before:absolute data-[tab-active='true']:before:-left-[.3em] data-[tab-active='true']:before:-right-[.3em] data-[tab-active='true']:before:bottom-0 data-[tab-active='true']:before:top-0 data-[tab-active='true']:before:-z-10 data-[tab-active='true']:before:rounded-md data-[tab-active='true']:before:bg-rose-950 data-[tab-active='true']:before:shadow-[inset_0_0_2px] data-[tab-active='true']:before:shadow-black`}
+  //         >
+  //           <span
+  //             data-tab-active={tab.key == item.id}
+  //             className="relative w-3 after:absolute after:inset-y-0 after:-right-2 after:w-[2px] after:rounded-full after:bg-rose-950 after:group-hover:bg-rose-900 data-[tab-active='true']:after:bg-rose-900"
+  //           >
+  //             {item.icon}
+  //           </span>
+  //           <span>{item.title}</span>
+  //         </div>
+  //       </div>,
+  //     )
+  //   })
 
-    return navTabs
-  }
+  //   return navTabs
+  // }
 }
---
+++ components/app-sidebar.js
@@ -0,0 +1,44 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarTrigger,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+export function AppSidebar(props) {
+  const items = props.items
+  return (
+    <div>
+      <SidebarProvider defaultOpen={false}>
+        <Sidebar collapsible="icon">
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupLabel>Application</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {items.map((item) => (
+                    <SidebarMenuItem key={item.id}>
+                      <SidebarMenuButton asChild>
+                        <a onClick={item.action}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+            <SidebarTrigger className="self-center" />
+          </SidebarContent>
+        </Sidebar>
+      </SidebarProvider>
+    </div>
+  )
+}
