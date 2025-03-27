82354ff59fcb623ba7b0323a786445c6b241e613
JulienStaples <julienstaples.web@gmail.com>
2025-03-26 21:50:54 -0700

    Feat: Add word component to db table

--- app/components/columns.js
+++ app/components/columns.js
@@ -1,9 +1,18 @@
 "use client"
 
+import Word from "./virtPage/Word"
+
 export const columns = [
   {
     accessorKey: "word",
     header: "word",
+    cell: ({ row }) => {
+      return (
+        <Word
+          wordObj={{ normal: row.getValue("word"), text: row.getValue("word") }}
+        />
+      )
+    },
   },
   {
     accessorKey: "def",
