dafccfcba73073ecc88b2b573f8b7d353fede0d8
JulienStaples <julienstaples.web@gmail.com>
2025-03-26 20:07:34 -0700

    Feat: Add basic shadcn table

---
+++ app/components/columns.js
@@ -0,0 +1,24 @@
+"use client"
+
+export const columns = [
+  {
+    accessorKey: "word",
+    header: "word",
+  },
+  {
+    accessorKey: "def",
+    header: "def",
+  },
+  {
+    accessorKey: "root",
+    header: "root",
+  },
+  {
+    accessorKey: "diff",
+    header: "diff",
+  },
+  {
+    accessorKey: "lang",
+    header: "lang",
+  },
+]
--- app/components/nav/DbSearchTab.js
+++ app/components/nav/DbSearchTab.js
@@ -1,169 +1,18 @@
 "use client"
 
-import React, { useEffect, useState } from "react"
 import { wordDb } from "../../lib/constants/constants"
-import Word from "../virtPage/Word"
 import { motion } from "framer-motion"
-import AppSelect from "@/components/app-select"
-import { Label } from "@/components/ui/label"
-import { Textarea } from "@/components/ui/textarea"
+import { DataTable } from "../data-table"
+import { columns } from "../columns"
 
-export default function DbSearchTab(props) {
-  let [response, setResponse] = useState([[]])
-  let [searchOption, setSearchOption] = useState(undefined)
-  let [included, setIncluded] = useState(undefined)
-  let [query, setQuery] = useState("")
-
-  useEffect(() => {
-    setResponse(
-      request({ option: searchOption, query: query, included: included }),
-    )
-  }, [searchOption, query, included])
-
-  function request(props) {
-    let option = props.option
-    let query = props.query
-    let included = props.included
-    let db = []
-
-    included ? (db = [...wordDb].filter(findIncluded)) : (db = [...wordDb])
-
-    let spans = []
-
-    option ? genSpans(colSearch()) : genSpans(db)
-
-    return spans
-
-    function genSpans(entries) {
-      entries.forEach((entry) => {
-        let key = entry[0]
-        let obj = entry[1]
-
-        spans.push(
-          <React.Fragment key={`${key}-word`}>
-            <span className="truncate">
-              {<Word wordObj={{ normal: obj.word, text: obj.word }} />}
-            </span>
-            <span className="truncate">{obj.def}</span>
-            <span className="truncate">{obj.root}</span>
-            <span className="truncate">{obj.lang}</span>
-            <span className="truncate">{obj.diff}</span>
-          </React.Fragment>,
-        )
-      })
-    }
-
-    function colSearch() {
-      return db.filter((entry) => entry[1][option] == query)
-    }
-
-    function findIncluded(entry) {
-      let key = entry[0]
-      let obj = entry[1]
-      return included.includes(obj.diff) || included.includes(obj.lang)
-    }
-  }
-
-  function searchOptionsAction(item) {
-    item.key == "all" ? setSearchOption(undefined) : setSearchOption(item.key)
-  }
-
-  function includesAction(item) {
-    if (item.key == "all") setIncluded(undefined)
-    else {
-      included
-        ? setIncluded((prev) => [...prev, item.key])
-        : setIncluded([item.key])
-    }
-  }
-
+export default function DbSearchTab() {
   return (
     <motion.div
-      id="db-tab"
-      className=""
+      id="texts-tab"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
-      <div className="flex w-full flex-col gap-6">
-        <div className="grid w-full gap-1.5">
-          <Label className="mb-1 h-fit" htmlFor={"searchBox"}>
-            {"Search"}
-          </Label>
-          <Textarea
-            id={"searchBox"}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-        </div>
-        <div className="flex justify-around">
-          <AppSelect
-            placeholder={"Search Option"}
-            action={searchOptionsAction}
-            items={genSearchOptions()}
-          />
-          <AppSelect
-            placeholder={"Include Any"}
-            action={includesAction}
-            items={genIncludes()}
-          />
-        </div>
-
-        <div className="grid grid-cols-5 overflow-x-scroll">
-          <span>word</span>
-          <span>def</span>
-          <span>root</span>
-          <span>lang</span>
-          <span>diff</span>
-          {response}
-        </div>
-      </div>
+      <DataTable columns={columns} data={[...wordDb].map((e) => e[1])} />
     </motion.div>
   )
-
-  function genSearchOptions() {
-    let searchOptions = new Map([
-      ["word", "Word"],
-      ["def", "def"],
-      ["root", "Root"],
-      ["lang", "Lang"],
-      ["all", "all"],
-    ])
-
-    let items = []
-
-    searchOptions.forEach((option, key) => {
-      let item = {
-        key: key,
-        text: option,
-      }
-
-      items.push(item)
-    })
-
-    return items
-  }
-
-  function genIncludes() {
-    let searchOptions = new Map([
-      ["all", "all"],
-      ["easy", "diff: easy"],
-      ["med", "diff: med"],
-      ["hard", "diff: hard"],
-      ["wk", "diff: wk"],
-      ["en", "lang: en"],
-      ["fr", "lang: fr"],
-    ])
-
-    let items = []
-
-    searchOptions.forEach((option, key) => {
-      let item = {
-        key: key,
-        text: option,
-      }
-
-      items.push(item)
-    })
-
-    return items
-  }
 }
