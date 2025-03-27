4858dbb9f5ec4b0f15183f49d147efbe3edba04b
JulienStaples <julienstaples.web@gmail.com>
2025-03-27 11:28:35 -0700

    Feat: Add tooltips to words

--- app/components/virtPage/Word.js
+++ app/components/virtPage/Word.js
@@ -4,10 +4,18 @@ import {
   findDiff,
   diffWordColors,
   findEntry,
+  wordDb,
 } from "@/app/lib/constants/constants"
 import { motion } from "framer-motion"
 import { spanVari, wordVari } from "../../lib/constants/virtPageAnims"
 import { NavContext } from "@/app/context/NavContext"
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export default function Word(props) {
   const wordObj = props.wordObj
@@ -30,23 +38,34 @@ export default function Word(props) {
 
   return (
     <>
-      <motion.span
-        id={wordObj.normal}
-        initial="init"
-        whileHover="hover"
-        variants={wordVari}
-        onClick={() => handleClick(wordObj)}
-        className={`group relative z-0 mr-[2px] inline-block cursor-pointer rounded-sm after:absolute after:inset-0 after:-inset-x-[.07em] after:inset-y-[.15em] after:rounded-sm after:shadow-md after:shadow-transparent hover:z-20 after:hover:shadow-black`}
-      >
-        <span className="word-span group-hover:invert">{wordObj.text}</span>
-        <motion.span
-          initial="init"
-          animate="enter"
-          variants={spanVari}
-          data-diff={wordDiff}
-          className={`bg-span ${diffWordColors} absolute -inset-x-[.07em] inset-y-[.15em] -z-10 origin-left rounded-sm group-hover:invert`}
-        />
-      </motion.span>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <motion.span
+              id={wordObj.normal}
+              initial="init"
+              whileHover="hover"
+              variants={wordVari}
+              onClick={() => handleClick(wordObj)}
+              className={`group relative z-0 mr-[2px] inline-block cursor-pointer rounded-sm after:absolute after:inset-0 after:-inset-x-[.07em] after:inset-y-[.15em] after:rounded-sm after:shadow-md after:shadow-transparent hover:z-20 after:hover:shadow-black`}
+            >
+              <span className="word-span group-hover:invert">
+                {wordObj.text}
+              </span>
+              <motion.span
+                initial="init"
+                animate="enter"
+                variants={spanVari}
+                data-diff={wordDiff}
+                className={`bg-span ${diffWordColors} absolute -inset-x-[.07em] inset-y-[.15em] -z-10 origin-left rounded-sm group-hover:invert`}
+              />
+            </motion.span>
+          </TooltipTrigger>
+          <TooltipContent className="mb-2">
+            <p>{wordDb.get(wordObj.normal)?.def ?? "..."}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <span>{wordObj.post}</span>
     </>
   )
