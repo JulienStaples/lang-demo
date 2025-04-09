21771a4b7d13710d5d1fe0e7681d4919e5b84ff0
JulienStaples <julienstaples.web@gmail.com>
2025-04-09 12:12:03 -0700

    Refac: Make selectTab more readable

--- app/context/NavContext.js
+++ app/context/NavContext.js
@@ -3,15 +3,28 @@
 import { createContext, useState } from "react"
 import TranslateTab from "../components/nav/TranslateTab"
 import DetailsTab from "../components/nav/DetailsTab"
-import DbSearch from "../components/nav/DbSearchTab"
+import DbSearchTab from "../components/nav/DbSearchTab"
 import { useAnimate } from "framer-motion"
 
 export const NavContext = createContext()
 
 export default function NavProvider({ children }) {
-  const [tab, setTab] = useState("")
+  const [tab, setTab] = useState()
   const [tabsPane, setTabsPane] = useState(false)
   const [scope, animate] = useAnimate()
+
+  const tabs = {
+    "translate-tab": TranslateTab,
+    "details-tab": DetailsTab,
+    "db-tab": DbSearchTab,
+  }
+
+  function selectTab(curTab) {
+    if (!tabsPane) setTabsPane(true)
+
+    const TabComponent = tabs[curTab]
+    setTab(<TabComponent key={curTab} />)
+  }
 
   function exitAnim() {
     const exitSeq = [[scope.current, { scaleX: 0 }, { duration: 0.07 }]]
@@ -21,17 +34,6 @@ export default function NavProvider({ children }) {
         setTabsPane(false)
       }, 0)
     })
-  }
-
-  function selectTab(curTab) {
-    if (!tabsPane) setTabsPane(true)
-
-    if (curTab === "translate-tab")
-      setTab(<TranslateTab key={"translate-tab"} selectTab={selectTab} />)
-    if (curTab === "details-tab")
-      setTab(<DetailsTab key={"details-tab"} selectTab={selectTab} />)
-    if (curTab === "db-tab")
-      setTab(<DbSearch key={"db-tab"} selectTab={selectTab} />)
   }
 
   return (
