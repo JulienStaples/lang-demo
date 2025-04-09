cbe10d751c77a86bccb80f9735eaab419bc0caf5
JulienStaples <julienstaples.web@gmail.com>
2025-04-09 12:13:55 -0700

    Sml: Remove tooltips

--- app/components/virtPage/Word.js
+++ app/components/virtPage/Word.js
@@ -38,34 +38,24 @@ export default function Word(props) {
 
   return (
     <>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <motion.span
-              id={wordObj.normal}
-              initial="init"
-              whileHover="hover"
-              variants={wordVari}
-              onClick={() => handleClick(wordObj)}
-              className={`group relative z-0 mr-[2px] inline-block cursor-pointer rounded-sm after:absolute after:inset-0 after:-inset-x-[.07em] after:inset-y-[.15em] after:rounded-sm after:shadow-md after:shadow-transparent hover:z-20 after:hover:shadow-black`}
-            >
-              <span className="word-span group-hover:invert">
-                {wordObj.text}
-              </span>
-              <motion.span
-                initial="init"
-                animate="enter"
-                variants={spanVari}
-                data-diff={wordDiff}
-                className={`bg-span ${diffWordColors} absolute -inset-x-[.07em] inset-y-[.15em] -z-10 origin-left rounded-sm group-hover:invert`}
-              />
-            </motion.span>
-          </TooltipTrigger>
-          <TooltipContent className="mb-2">
-            <p>{wordDb.get(wordObj.normal)?.def ?? "..."}</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <motion.span
+        id={wordObj.normal}
+        initial="init"
+        whileHover="hover"
+        variants={wordVari}
+        onClick={() => handleClick(wordObj)}
+        className={`group relative z-0 mr-[2px] inline-block cursor-pointer rounded-sm after:absolute after:inset-0 after:-inset-x-[.07em] after:inset-y-[.15em] after:rounded-sm after:shadow-md after:shadow-transparent hover:z-20 after:hover:shadow-black`}
+      >
+        <span className="word-span group-hover:invert">{wordObj.text}</span>
+        <motion.span
+          initial="init"
+          animate="enter"
+          variants={spanVari}
+          data-diff={wordDiff}
+          className={`bg-span ${diffWordColors} absolute -inset-x-[.07em] inset-y-[.15em] -z-10 origin-left rounded-sm group-hover:invert`}
+        />
+      </motion.span>
+
       <span>{wordObj.post}</span>
     </>
   )
