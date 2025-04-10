879ceb9ad9742883ace87e096f65ea180e82185c
JulienStaples <julienstaples.web@gmail.com>
2025-04-10 13:18:08 -0700

    Refac: Make dbTab more readable

--- app/components/nav/dbTab/DbTab.js
+++ app/components/nav/dbTab/DbTab.js
@@ -8,7 +8,7 @@ import { columns } from "./columns"
 export default function DbSearchTab() {
   return (
     <motion.div id="db-tab" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-      <DataTable columns={columns} data={[...wordDb].map((e) => e[1])} />
+      <DataTable columns={columns} data={[...wordDb.values()]} />
     </motion.div>
   )
 }
