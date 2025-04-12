3d89b64ecee4711870740cadf4d5dad14f0957d4
JulienStaples <julienstaples.web@gmail.com>
2025-04-12 12:25:27 -0700

    Feat: Add ability to change word language

--- app/components/nav/TranslateTab.js
+++ app/components/nav/TranslateTab.js
@@ -9,9 +9,10 @@ import { motion } from "framer-motion"
 import AppTextarea from "@/components/app-textarea"
 import { Button } from "@/components/ui/button"
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+import AppSelect from "@/components/app-select"
 
 export default function TranslateTab() {
-  const { activeWordObj, langOption } = useContext(AppContext)
+  const { activeWordObj, langOption, presetText } = useContext(AppContext)
   const { exitAnim } = useContext(NavContext)
   const defBox = useRef()
   const parentBox = useRef()
@@ -19,18 +20,19 @@ export default function TranslateTab() {
   const activeWord = activeWordObj.normal
   const activeEntry = wordDb.get(activeWord)
   const [uiDiff, setUiDiff] = useState(activeEntry?.diff ?? undefined)
+  const [wordLang, setWordLang] = useState(activeEntry?.lang ?? presetText.lang)
 
   sessionStorage.setItem("wordDb", JSON.stringify([...wordDb]))
 
   function saveEntry(diff) {
-    let newDiff = diff || (activeEntry?.diff ?? "hard")
+    const newDiff = diff || (activeEntry?.diff ?? "hard")
 
     const newEntryObj = {
       word: activeWordObj.text,
       def: defBox.current.value || undefined,
       parent: parentBox.current.value || undefined,
       diff: newDiff,
-      lang: "lang",
+      lang: wordLang,
     }
 
     wordDb.set(activeWord, newEntryObj)
@@ -51,6 +53,16 @@ export default function TranslateTab() {
 
     activeEntry.diff = diff
     setUiDiff(diff)
+  }
+
+  const langItems = [
+    { key: "en", text: "en" },
+    { key: "fr", text: "fr" },
+    { key: "la", text: "la" },
+  ]
+
+  function changeWordLang({ key }) {
+    setWordLang(key)
   }
 
   return (
@@ -78,41 +90,49 @@ export default function TranslateTab() {
           label={`Parent: ${activeEntry?.parent ?? "..."}`}
           defaultValue={activeEntry?.parent ?? ""}
         />
-        <ToggleGroup
-          className="w-fit gap-0 self-start rounded-sm border"
-          type="single"
-          value={uiDiff}
-          onValueChange={(value) => handleDiffChange(value)}
-        >
-          <ToggleGroupItem
-            value="wk"
-            className={`rounded-l-sm rounded-r-none border-foreground bg-transparent`}
-            aria-label="Toggle wk"
+        <div className="flex gap-4">
+          <ToggleGroup
+            className="w-fit gap-0 self-start rounded-sm border"
+            type="single"
+            value={uiDiff}
+            onValueChange={(value) => handleDiffChange(value)}
           >
-            wk
-          </ToggleGroupItem>
-          <ToggleGroupItem
-            value="easy"
-            className={`${diffBtnColors.easy} rounded-none`}
-            aria-label="Toggle easy"
-          >
-            easy
-          </ToggleGroupItem>
-          <ToggleGroupItem
-            value="med"
-            className={`${diffBtnColors.med} rounded-none`}
-            aria-label="Toggle med"
-          >
-            med
-          </ToggleGroupItem>
-          <ToggleGroupItem
-            value="hard"
-            className={`${diffBtnColors.hard} rounded-l-none rounded-r-sm`}
-            aria-label="Toggle hard"
-          >
-            hard
-          </ToggleGroupItem>
-        </ToggleGroup>
+            <ToggleGroupItem
+              value="wk"
+              className={`rounded-l-sm rounded-r-none border-foreground bg-transparent`}
+              aria-label="Toggle wk"
+            >
+              wk
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              value="easy"
+              className={`${diffBtnColors.easy} rounded-none`}
+              aria-label="Toggle easy"
+            >
+              easy
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              value="med"
+              className={`${diffBtnColors.med} rounded-none`}
+              aria-label="Toggle med"
+            >
+              med
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              value="hard"
+              className={`${diffBtnColors.hard} rounded-l-none rounded-r-sm`}
+              aria-label="Toggle hard"
+            >
+              hard
+            </ToggleGroupItem>
+          </ToggleGroup>
+
+          <AppSelect
+            placeholder={wordLang}
+            items={langItems}
+            action={changeWordLang}
+          />
+        </div>
       </div>
 
       <div className="flex flex-col gap-4">
