d51b030dddecfbeecd9f37050abfaf5a3361e8da
JulienStaples <julienstaples.web@gmail.com>
2025-11-12 08:19:03 -0700

    feat: add caching to fetch

--- src/utils/fetchTranslation.tsx
+++ src/utils/fetchTranslation.tsx
@@ -1,19 +1,23 @@
 "use server"
 import { TranslationObj } from "@/types/types"
 
-export default async function fetchTranslation(TranslateObj: TranslationObj) {
+export default async function fetchTranslation(props: TranslationObj) {
+  const { text, source, target } = props
+
   const response = await fetch("https://api-free.deepl.com/v2/translate", {
+    cache: "force-cache",
     method: "POST",
     headers: {
       Authorization: `DeepL-Auth-Key ${process.env.DEEPL_AUTH_KEY}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      text: TranslateObj.text,
-      source_lang: TranslateObj.source,
-      target_lang: TranslateObj.target,
+      text: text,
+      source_lang: source,
+      target_lang: target,
     }),
   })
+
   const data = await response.json()
   return data.translations[0].text
 }
