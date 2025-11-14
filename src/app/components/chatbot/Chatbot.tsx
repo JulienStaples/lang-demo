2d98c88e5089c1fe69c7a33ec37121ae33db95bc
JulienStaples <julienstaples.web@gmail.com>
2025-11-14 09:36:25 -0700

    feat: make chatbot responsive

--- src/app/components/chatbot/Chatbot.tsx
+++ src/app/components/chatbot/Chatbot.tsx
@@ -1,18 +1,34 @@
 "use client"
 
+import { useIsMobile } from "@/hooks/use-mobile"
 import { BotMessageSquare } from "lucide-react"
 import React, { useState } from "react"
 
 export default function Chatbot() {
   const [showChat, setShowChat] = useState<boolean>(false)
+  const isMobile = useIsMobile()
+
+  function sizeChatWindow() {
+    const mobileStyles = "h-[90%] w-[100%] flex-col"
+    const desktopStyles = "h-[83%] w-3/5 "
+
+    return (isMobile && mobileStyles) || desktopStyles
+  }
+
+  function positionChatContainer() {
+    const mobileStyles = "bottom-0 right-0"
+    const desktopStyles = "bottom-2 mr-2"
+
+    return (isMobile && mobileStyles) || desktopStyles
+  }
 
   return (
     <div
-      className={`absolute bottom-0 right-0 z-50 mb-16 mr-4 flex ${(showChat && "h-[83%]") || "h-min"} ${(showChat && "w-3/5") || "w-0-min"} items-end gap-2`}
+      className={`absolute bottom-0 right-0 z-50 mb-16 mr-4 flex items-end ${(showChat && sizeChatWindow()) || "w-0-min h-min"} gap-2`}
     >
       {showChat && (
         <div
-          className={`relative bottom-2 mr-2 h-full w-full overflow-hidden rounded-[37px] p-2`}
+          className={`relative ${positionChatContainer()} h-full w-full overflow-hidden rounded-[37px] p-2`}
           id={"bp-embedded-webchat"}
         />
       )}
